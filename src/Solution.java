54183fd2e184c843703baa26e02ab1d72e2b8b88
Thomas Young <newtype0092@163.com>
2020-08-16 18:37:21 +0800

    solution template

---
+++ src/Solution.java
@@ -0,0 +1,36 @@
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+
+public class Solution {
+    public ArrayList intputs;
+    public ArrayList outputs;
+
+    public Boolean test(int i) {
+        Method[] methods = this.getClass().getDeclaredMethods();
+        Method m = methods[0];
+
+        System.out.println(m.getName());
+
+        Parameter[] parameters = m.getParameters();
+        for (Parameter p:parameters) {
+            Type t = p.getType();
+            System.out.println(p.getName() + " " + p.getType());
+        }
+
+        try {
+            ArrayList<Object> ar = (ArrayList<Object>) intputs.get(i);
+            var ret = m.invoke(this, ar.toArray());
+            System.out.println("result:" + ret);
+            return outputs.get(i).equals(ret);
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+
+        return false;
+    }
+}
