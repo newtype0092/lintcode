1e9b15918c20d84ad53a9d4a85def60653b5ec1e
Thomas Young <newtype0092@163.com>
2020-08-19 17:12:20 +0800

    comment

--- src/LintCode.java
+++ src/LintCode.java
@@ -1,4 +1,6 @@
 public class LintCode {
     public static void main(String[] args) {
+//        boolean success = (new SolutionXXX()).test(caseIdx);
+//        System.out.println(success);
     }
 }
